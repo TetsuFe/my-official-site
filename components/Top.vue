1028106b43ef4c5f4cbf8554ddc259f6ab7586da
TetsuFe <tetsubeg1ner27@gmail.com>
2019-06-22 15:00:07 +0900

    Add Services title on portfolio carousel

--- components/Top.vue
+++ components/Top.vue
@@ -1,5 +1,9 @@
 <template>
   <section id="top">
+    <div class="contents-title">
+      <p>サービス</p>
+      <h3>Services</h3>
+    </div>
   <carousel per-page="1" mouse-drag="false" >
     <slide>
       <div class="img-wrap">
@@ -34,6 +38,53 @@
     max-width: 80%;
     max-height: 700px;
     object-fit: contain;
+  }
+
+  .contents-title {
+    margin-bottom: -100px;
+  }
+
+  .contents-title h3 {
+    width: 224px;
+    font-family: Arvo;
+    font-weight: 700;
+    font-size: 25px;
+    line-height: 1.8;
+    letter-spacing: 7px;
+    text-align: center;
+    padding-left: 7px;
+    margin: 0 auto 80px;
+  }
+
+  .contents-title p {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 1;
+    letter-spacing: 6px;
+    text-align: center;
+    padding-left: 6px;
+  }
+
+  #wrap.sideA .contents-title h3 {
+    color: #50aa32!important;
+    border-bottom: 4px solid #50aa32;
+  }
+
+  #wrap.sideB .contents-title h3 {
+    color: black!important;
+    border-bottom: 4px solid black;
+  }
+
+  #wrap.sideA .contents-title p {
+    color: #50aa32!important;
+  }
+
+  #wrap.sideB .contents-title p {
+    color: #ffe800!important;
+  }
+
+  #top {
+    margin-top: 70px;
   }
 
 </style>
